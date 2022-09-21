c23247cdecdd77a5b64419ed0286795c595ac1a5
Alex Sauer <sauer.alexander@gmx.de>
2022-09-21 16:55:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MyUtils
+
+This is a simple package that collects some functions which I often use
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='myutils',
+      version='1.0',
+      # list folders, not files
+      packages=['myutils']
+	  )
